07b44710d36b2c079d298fddb9712826824375eb
Meteey <100533901+Meteey@users.noreply.github.com>
2023-08-16 19:06:22 +0300

    Add files via upload
    16.08.2023 tarihinde yazdığım
    https://mustafakonur.com/2023/08/16/flutterda-http-istekleri-nasil-gonderilir/
    adresli yazıda bulunan kodlar

---
+++ server_test.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:benim_avukatim/LoginPages/value_sender.dart';
+import 'package:flutter/material.dart';
+class ServerTest extends StatefulWidget {
+  @override
+  State<ServerTest> createState() => _ServerTestState();
+}
+class _ServerTestState extends State<ServerTest> {
+  TextEditingController txtcontroller = TextEditingController();
+  String response = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          TextFormField(controller: txtcontroller,),
+          Text("Sonuç: $response")
+        ],
+      ),
+     floatingActionButton: FloatingActionButton(onPressed: ()async{
+      ServerConnectionExample serverConnectionExample = ServerConnectionExample(value: txtcontroller.text);
+      response = await serverConnectionExample.postData(serverConnectionExample);
+      setState(() {
+      });
+     }),
+    );
+  }
+}
